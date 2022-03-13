09d191a16cc5c94773f9ab5b4398772da4548f34
Jesus-LinaresIT <alberto.salinas@asesoriait.com>
2022-03-12 19:12:02 -0600

    Create conection to database with DPO class

---
+++ MVC/Models/conectionsDB.php
@@ -0,0 +1,12 @@
+<?php
+
+Class Conections{
+
+   static public function conect(){
+
+      $link = new PDO("mysql:host=localhost;dbname=users","root","");
+      $link->exec('set names utf8');
+   }
+}
+
+
--- MVC/index.php
+++ MVC/index.php
@@ -3,5 +3,11 @@
 require_once "controllers/layout.controller.php";
 require_once "controllers/form.controller.php";
 
+require_once "Models/conectionsDB.php";
+
+
+$conections = Conections::conect();
+echo '<pre>'; print_r($conections); echo '</pre>';
+
 $layout = new ControllerLayout();
 $layout -> ctrGetLayout();
